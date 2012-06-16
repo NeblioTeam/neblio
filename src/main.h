27807946d3fd2d710c73dbb3fc02e3db2183bb27
Pieter Wuille <pieter.wuille@gmail.com>
2012-06-16 13:36:00 +0200

    Compact serialization for amounts
    Special serializer/deserializer for amount values. It is optimized for
    values which have few non-zero digits in decimal representation. Most
    amounts currently in the txout set take only 1 or 2 bytes to
    represent.

--- src/main.h
+++ src/main.h
@@ -694,14 +694,25 @@ class CTxOutCompressor
 {
 private:
     CTxOut &txout;
+
 public:
+    static uint64 CompressAmount(uint64 nAmount);
+    static uint64 DecompressAmount(uint64 nAmount);
+
     CTxOutCompressor(CTxOut &txoutIn) : txout(txoutIn) { }
 
-    IMPLEMENT_SERIALIZE(
-        READWRITE(VARINT(txout.nValue));
+    IMPLEMENT_SERIALIZE(({
+        if (!fRead) {
+            uint64 nVal = CompressAmount(txout.nValue);
+            READWRITE(VARINT(nVal));
+        } else {
+            uint64 nVal = 0;
+            READWRITE(VARINT(nVal));
+            txout.nValue = DecompressAmount(nVal);
+        }
         CScriptCompressor cscript(REF(txout.scriptPubKey));
         READWRITE(cscript);
-    )
+    });)
 };
 
 bool IsFinalTx(const CTransaction &tx, int nBlockHeight = 0, int64_t nBlockTime = 0);
