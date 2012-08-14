3636c67655e27c6cfbc286da50cbba986d3d1d88
Matt Corallo <git@bluematt.me>
2012-08-15 01:21:20 +0200

    Add a CBlock.GetBlockHeader

--- src/main.h
+++ src/main.h
@@ -942,6 +942,18 @@ public:
         return maxTransactionTime;
     }
 
+    CBlockHeader GetBlockHeader() const
+    {
+        CBlockHeader block;
+        block.nVersion       = nVersion;
+        block.hashPrevBlock  = hashPrevBlock;
+        block.hashMerkleRoot = hashMerkleRoot;
+        block.nTime          = nTime;
+        block.nBits          = nBits;
+        block.nNonce         = nNonce;
+        return block;
+    }
+
     uint256 BuildMerkleTree() const
     {
         vMerkleTree.clear();
