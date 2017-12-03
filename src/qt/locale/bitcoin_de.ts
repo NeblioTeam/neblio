8e9f372ffb8423bac8b6ce57b010d251bf6c1762
Sebastian Rückoldt <sebrock@gmail.com>
2017-12-03 04:40:30 +0100

    Update bitcoin_de.ts
    quality control 1

--- src/qt/locale/bitcoin_de.ts
+++ src/qt/locale/bitcoin_de.ts
@@ -1,7 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="de" version="2.1">
-<context>
-    <name>AboutDialog</name>
-    <message><?xml version="1.0" ?><!DOCTYPE TS><TS language="de" version="2.1">
+<?xml version="1.0" ?>
+<!DOCTYPE TS>
+<TS language="de" version="2.1">
 <context>
     <name>AboutDialog</name>
     <message>
