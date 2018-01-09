7eb472a5b747a1abb6574fdb012a7f3cce611bab
Samer Afach <samer@afach.de>
2018-01-09 13:46:16 +0100

    Moved all GUI forms to header files through UIC.

---
+++ src/qt/ui_addressbookpage.h
@@ -0,0 +1,183 @@
+/********************************************************************************
+** Form generated from reading UI file 'addressbookpage.ui'
+**
+** Created by: Qt User Interface Compiler version 5.7.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_ADDRESSBOOKPAGE_H
+#define UI_ADDRESSBOOKPAGE_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QDialogButtonBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QTableView>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_AddressBookPage
+{
+public:
+    QVBoxLayout *verticalLayout;
+    QLabel *labelExplanation;
+    QTableView *tableView;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *newAddressButton;
+    QPushButton *copyToClipboard;
+    QPushButton *showQRCode;
+    QPushButton *signMessage;
+    QPushButton *verifyMessage;
+    QPushButton *deleteButton;
+    QSpacerItem *horizontalSpacer;
+    QDialogButtonBox *buttonBox;
+
+    void setupUi(QWidget *AddressBookPage)
+    {
+        if (AddressBookPage->objectName().isEmpty())
+            AddressBookPage->setObjectName(QStringLiteral("AddressBookPage"));
+        AddressBookPage->resize(760, 380);
+        AddressBookPage->setStyleSheet(QStringLiteral("background-image: url(:/images/gr);"));
+        verticalLayout = new QVBoxLayout(AddressBookPage);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        labelExplanation = new QLabel(AddressBookPage);
+        labelExplanation->setObjectName(QStringLiteral("labelExplanation"));
+        labelExplanation->setTextFormat(Qt::PlainText);
+        labelExplanation->setWordWrap(true);
+
+        verticalLayout->addWidget(labelExplanation);
+
+        tableView = new QTableView(AddressBookPage);
+        tableView->setObjectName(QStringLiteral("tableView"));
+        tableView->setContextMenuPolicy(Qt::CustomContextMenu);
+        tableView->setTabKeyNavigation(false);
+        tableView->setAlternatingRowColors(true);
+        tableView->setSelectionMode(QAbstractItemView::SingleSelection);
+        tableView->setSelectionBehavior(QAbstractItemView::SelectRows);
+        tableView->setSortingEnabled(true);
+        tableView->verticalHeader()->setVisible(false);
+
+        verticalLayout->addWidget(tableView);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        newAddressButton = new QPushButton(AddressBookPage);
+        newAddressButton->setObjectName(QStringLiteral("newAddressButton"));
+        QIcon icon;
+        icon.addFile(QStringLiteral(":/icons/add"), QSize(), QIcon::Normal, QIcon::Off);
+        newAddressButton->setIcon(icon);
+
+        horizontalLayout->addWidget(newAddressButton);
+
+        copyToClipboard = new QPushButton(AddressBookPage);
+        copyToClipboard->setObjectName(QStringLiteral("copyToClipboard"));
+        QIcon icon1;
+        icon1.addFile(QStringLiteral(":/icons/editcopy"), QSize(), QIcon::Normal, QIcon::Off);
+        copyToClipboard->setIcon(icon1);
+
+        horizontalLayout->addWidget(copyToClipboard);
+
+        showQRCode = new QPushButton(AddressBookPage);
+        showQRCode->setObjectName(QStringLiteral("showQRCode"));
+        QIcon icon2;
+        icon2.addFile(QStringLiteral(":/icons/qrcode"), QSize(), QIcon::Normal, QIcon::Off);
+        showQRCode->setIcon(icon2);
+
+        horizontalLayout->addWidget(showQRCode);
+
+        signMessage = new QPushButton(AddressBookPage);
+        signMessage->setObjectName(QStringLiteral("signMessage"));
+        QIcon icon3;
+        icon3.addFile(QStringLiteral(":/icons/edit"), QSize(), QIcon::Normal, QIcon::Off);
+        signMessage->setIcon(icon3);
+
+        horizontalLayout->addWidget(signMessage);
+
+        verifyMessage = new QPushButton(AddressBookPage);
+        verifyMessage->setObjectName(QStringLiteral("verifyMessage"));
+        QIcon icon4;
+        icon4.addFile(QStringLiteral(":/icons/transaction_0"), QSize(), QIcon::Normal, QIcon::Off);
+        verifyMessage->setIcon(icon4);
+
+        horizontalLayout->addWidget(verifyMessage);
+
+        deleteButton = new QPushButton(AddressBookPage);
+        deleteButton->setObjectName(QStringLiteral("deleteButton"));
+        QIcon icon5;
+        icon5.addFile(QStringLiteral(":/icons/remove"), QSize(), QIcon::Normal, QIcon::Off);
+        deleteButton->setIcon(icon5);
+
+        horizontalLayout->addWidget(deleteButton);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer);
+
+        buttonBox = new QDialogButtonBox(AddressBookPage);
+        buttonBox->setObjectName(QStringLiteral("buttonBox"));
+        QSizePolicy sizePolicy(QSizePolicy::Maximum, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(buttonBox->sizePolicy().hasHeightForWidth());
+        buttonBox->setSizePolicy(sizePolicy);
+        buttonBox->setStandardButtons(QDialogButtonBox::Ok);
+
+        horizontalLayout->addWidget(buttonBox);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+
+        retranslateUi(AddressBookPage);
+
+        QMetaObject::connectSlotsByName(AddressBookPage);
+    } // setupUi
+
+    void retranslateUi(QWidget *AddressBookPage)
+    {
+        AddressBookPage->setWindowTitle(QApplication::translate("AddressBookPage", "Address Book", Q_NULLPTR));
+        labelExplanation->setText(QApplication::translate("AddressBookPage", "These are your neblio addresses for receiving payments. You may want to give a different one to each sender so you can keep track of who is paying you.", Q_NULLPTR));
+#ifndef QT_NO_TOOLTIP
+        tableView->setToolTip(QApplication::translate("AddressBookPage", "Double-click to edit address or label", Q_NULLPTR));
+#endif // QT_NO_TOOLTIP
+#ifndef QT_NO_TOOLTIP
+        newAddressButton->setToolTip(QApplication::translate("AddressBookPage", "Create a new address", Q_NULLPTR));
+#endif // QT_NO_TOOLTIP
+        newAddressButton->setText(QApplication::translate("AddressBookPage", "&New Address", Q_NULLPTR));
+#ifndef QT_NO_TOOLTIP
+        copyToClipboard->setToolTip(QApplication::translate("AddressBookPage", "Copy the currently selected address to the system clipboard", Q_NULLPTR));
+#endif // QT_NO_TOOLTIP
+        copyToClipboard->setText(QApplication::translate("AddressBookPage", "&Copy Address", Q_NULLPTR));
+        showQRCode->setText(QApplication::translate("AddressBookPage", "Show &QR Code", Q_NULLPTR));
+#ifndef QT_NO_TOOLTIP
+        signMessage->setToolTip(QApplication::translate("AddressBookPage", "Sign a message to prove you own a neblio address", Q_NULLPTR));
+#endif // QT_NO_TOOLTIP
+        signMessage->setText(QApplication::translate("AddressBookPage", "Sign &Message", Q_NULLPTR));
+#ifndef QT_NO_TOOLTIP
+        verifyMessage->setToolTip(QApplication::translate("AddressBookPage", "Verify a message to ensure it was signed with a specified neblio address", Q_NULLPTR));
+#endif // QT_NO_TOOLTIP
+        verifyMessage->setText(QApplication::translate("AddressBookPage", "&Verify Message", Q_NULLPTR));
+#ifndef QT_NO_TOOLTIP
+        deleteButton->setToolTip(QApplication::translate("AddressBookPage", "Delete the currently selected address from the list", Q_NULLPTR));
+#endif // QT_NO_TOOLTIP
+        deleteButton->setText(QApplication::translate("AddressBookPage", "&Delete", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class AddressBookPage: public Ui_AddressBookPage {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_ADDRESSBOOKPAGE_H
