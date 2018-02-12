0c3c31006bb3b976d36e55e801ad10c09dd3d86c
Samer Afach <info@afach.de>
2018-02-12 20:53:09 +0100

    A segmentation fault was caused by accessing vector elements that may not exist. A condition that tests the size of the vector is added now.

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -1429,10 +1429,12 @@ Value walletpassphrase(const Array& params, bool fHelp)
             "walletpassphrase <passphrase> <timeout> [stakingonly]\n"
             "Stores the wallet decryption key in memory for <timeout> seconds.\n"
             "if [stakingonly] is true sending functions are disabled.");
-    if (params[1].get_int64() > 99999999 || params[1].get_int64() < 0) {
-    	throw runtime_error(
-            "walletpassphrase <passphrase> <timeout> [stakingonly]\n"
-            "Maximum timeout value is 99999999 use timeout of 0 to never re-lock, negative values not allowed");
+    if(params.size() > 2) {
+        if (params[1].get_int64() > 99999999 || params[1].get_int64() < 0) {
+            throw runtime_error(
+                        "walletpassphrase <passphrase> <timeout> [stakingonly]\n"
+                        "Maximum timeout value is 99999999 use timeout of 0 to never re-lock, negative values not allowed");
+        }
     }
     if (fHelp)
         return true;
