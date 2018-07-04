0c22d7a61c80cf93fa165ac5e75772d7a65ac14a
NeblioTeam <neblioteam@gmail.com>
2018-07-03 20:02:09 -0400

    Update OP_RETURN size tests

--- wallet/test/transaction_tests.cpp
+++ wallet/test/transaction_tests.cpp
@@ -282,30 +282,30 @@ TEST(transaction_tests, op_return_size)
         EXPECT_TRUE(IsStandardTx(t, reason)) << reason;
 
         // 80 bytes (exactly the allowed data size)
-        t.vout[0].scriptPubKey = CScript() << OP_RETURN << ParseHex(GeneratePseudoRandomHex(2*80));
+        t.vout[0].scriptPubKey = CScript() << OP_RETURN << ParseHex(GeneratePseudoRandomHex(2*4096));
         EXPECT_TRUE(IsStandardTx(t, reason)) << reason;
 
         // 81 bytes (1-byte over the limit)
-        t.vout[0].scriptPubKey = CScript() << OP_RETURN << ParseHex(GeneratePseudoRandomHex(2*81));
+        t.vout[0].scriptPubKey = CScript() << OP_RETURN << ParseHex(GeneratePseudoRandomHex(2*4097));
         EXPECT_FALSE(IsStandardTx(t, reason)) << reason;
 
 
         // Only one TX_NULL_DATA permitted in all cases
         t.vout.resize(2);
         t.vout[0].scriptPubKey = GetScriptForDestination(key.GetPubKey().GetID());;
-        t.vout[1].scriptPubKey = CScript() << OP_RETURN << ParseHex(GeneratePseudoRandomHex(2*80));
+        t.vout[1].scriptPubKey = CScript() << OP_RETURN << ParseHex(GeneratePseudoRandomHex(2*4096));
         EXPECT_TRUE(IsStandardTx(t, reason)) << reason;
 
         t.vout.resize(2);
-        t.vout[0].scriptPubKey = CScript() << OP_RETURN << ParseHex(GeneratePseudoRandomHex(2*80));
+        t.vout[0].scriptPubKey = CScript() << OP_RETURN << ParseHex(GeneratePseudoRandomHex(2*4096));
         t.vout[1].scriptPubKey = GetScriptForDestination(key.GetPubKey().GetID());;
         EXPECT_TRUE(IsStandardTx(t, reason)) << reason;
 
-        t.vout[0].scriptPubKey = CScript() << OP_RETURN << ParseHex(GeneratePseudoRandomHex(2*80));
-        t.vout[1].scriptPubKey = CScript() << OP_RETURN << ParseHex(GeneratePseudoRandomHex(2*80));
+        t.vout[0].scriptPubKey = CScript() << OP_RETURN << ParseHex(GeneratePseudoRandomHex(2*4096));
+        t.vout[1].scriptPubKey = CScript() << OP_RETURN << ParseHex(GeneratePseudoRandomHex(2*4096));
         EXPECT_FALSE(IsStandardTx(t, reason)) << reason;
 
-        t.vout[0].scriptPubKey = CScript() << OP_RETURN << ParseHex(GeneratePseudoRandomHex(2*80));
+        t.vout[0].scriptPubKey = CScript() << OP_RETURN << ParseHex(GeneratePseudoRandomHex(2*4096));
         t.vout[1].scriptPubKey = CScript() << OP_RETURN;
         EXPECT_FALSE(IsStandardTx(t, reason)) << reason;
 
