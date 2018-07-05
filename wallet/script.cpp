956a742bc41e96399edd2d1bfdff962c756e7cdd
NeblioTeam <neblioteam@gmail.com>
2018-07-05 19:37:03 -0400

    Up OP_RETURN max size

--- wallet/script.cpp
+++ wallet/script.cpp
@@ -1393,8 +1393,8 @@ bool Solver(const CScript& scriptPubKey, txnouttype& typeRet, vector<vector<unsi
             }
             else if (opcode2 == OP_SMALLDATA)
             {
-                // small pushdata, <= 80 bytes
-                if (vch1.size() > 80)
+                // small pushdata, <= 4096 bytes
+                if (vch1.size() > 4096)
                     break;
             }
             else if (opcode1 != opcode2 || vch1 != vch2)
