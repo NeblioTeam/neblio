23bcacfa5e487ddd170e1e407653ab3c01c7010b
NeblioTeam <neblioteam@gmail.com>
2018-10-04 18:38:46 -0400

    Build OpenSSL and cURL for neblio-Qt as well

--- ci_scripts/test_linux-gui_wallet.py
+++ ci_scripts/test_linux-gui_wallet.py
@@ -33,6 +33,14 @@ nci.install_packages_debian(packages_to_install)
 
 nci.mkdir_p(build_dir)
 os.chdir(build_dir)
+
+nci.call_with_err_code('python $TRAVIS_BUILD_DIR/build_scripts/CompileOpenSSL-Linux.py')
+nci.call_with_err_code('python $TRAVIS_BUILD_DIR/build_scripts/CompileCurl-Linux.py')
+
+os.environ['PKG_CONFIG_PATH'] = os.path.join(working_dir, 'curl_build/lib/pkgconfig/')
+os.environ['OPENSSL_INCLUDE_PATH'] = os.path.join(working_dir, '/openssl_build/include/')
+os.environ['OPENSSL_LIB_PATH'] = os.path.join(working_dir, 'openssl_build/lib/')
+
 nci.call_with_err_code('qmake "USE_UPNP=1" "USE_QRCODE=0" "RELEASE=1" "NEBLIO_CONFIG += Tests" ../neblio-wallet.pro')
 nci.call_with_err_code("make -j3")
 
