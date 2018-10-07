0eeeb38a1ee2df7681c99b7c6647b94e04cfdf56
NeblioTeam <neblioteam@gmail.com>
2018-10-07 15:04:23 -0400

    travis osx

---
+++ ci_scripts/test_osx-gui-wallet.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import neblio_ci_libs as nci
+
+working_dir = os.getcwd()
+build_dir = "build"
+
+nci.mkdir_p(build_dir)
+os.chdir(build_dir)
+
+nci.call_with_err_code('qmake "USE_UPNP=1" "USE_QRCODE=0" "RELEASE=1" "NEBLIO_CONFIG += Tests" ../neblio-wallet.pro')
+nci.call_with_err_code("make -j3")
+
+# run tests
+nci.call_with_err_code("./wallet/test/neblio-tests")
+
+print("")
+print("")
+print("Building finished successfully.")
+print("")
