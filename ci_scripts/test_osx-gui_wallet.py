36d11d119d0feb7cc7290a4acc4a6235417711a5
NeblioTeam <neblioteam@gmail.com>
2018-10-07 16:42:04 -0400

    move osx package installs to python build script

--- ci_scripts/test_osx-gui_wallet.py
+++ ci_scripts/test_osx-gui_wallet.py
@@ -11,6 +11,11 @@ build_dir = "build"
 nci.mkdir_p(build_dir)
 os.chdir(build_dir)
 
+nci.call_with_err_code('brew update')
+nci.call_with_err_code('brew install qt')
+nci.call_with_err_code('brew link --force qt')
+nci.call_with_err_code('brew install berkeley-db@4')
+
 nci.call_with_err_code('qmake "USE_UPNP=1" "USE_QRCODE=0" "RELEASE=1" "NEBLIO_CONFIG += Tests" ../neblio-wallet.pro')
 nci.call_with_err_code("make -j" + str(mp.cpu_count()))
 
