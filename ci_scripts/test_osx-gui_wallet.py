e541050509f96c253a6f9801e5d5c63f40a96f6d
NeblioTeam <neblioteam@gmail.com>
2018-10-07 17:13:07 -0400

    --overwrite all brew bottles

--- ci_scripts/test_osx-gui_wallet.py
+++ ci_scripts/test_osx-gui_wallet.py
@@ -12,9 +12,9 @@ nci.mkdir_p(build_dir)
 os.chdir(build_dir)
 
 nci.call_with_err_code('brew update')
-nci.call_with_err_code('brew install qt && brew link --force qt')
-nci.call_with_err_code('brew install berkeley-db@4 && brew link --force berkeley-db@4')
-nci.call_with_err_code('brew install boost@1.60 && brew link --force boost@1.60')
+nci.call_with_err_code('brew install qt && brew link --force --overwrite qt')
+nci.call_with_err_code('brew install berkeley-db@4 && brew link --force --overwrite berkeley-db@4')
+nci.call_with_err_code('brew install boost@1.60 && brew link --force --overwrite boost@1.60')
 
 nci.call_with_err_code('qmake "USE_UPNP=1" "USE_QRCODE=0" "RELEASE=1" "NEBLIO_CONFIG += Tests" ../neblio-wallet.pro')
 nci.call_with_err_code("make -j" + str(mp.cpu_count()))
