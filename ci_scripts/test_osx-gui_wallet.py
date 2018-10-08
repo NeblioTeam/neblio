656dfdbacab46f1c2640318805c9d6740cea7975
NeblioTeam <neblioteam@gmail.com>
2018-10-08 10:04:46 -0400

    debug

--- ci_scripts/test_osx-gui_wallet.py
+++ ci_scripts/test_osx-gui_wallet.py
@@ -45,7 +45,7 @@ nci.call_with_err_code('qmake "USE_UPNP=1" "USE_QRCODE=1" "RELEASE=1" "NEBLIO_CO
 nci.call_with_err_code("make -j" + str(mp.cpu_count()))
 
 nci.call_with_err_code('pwd')
-nci.call_with_err_code('ls -al')
+nci.call_with_err_code('ls -alR')
 
 nci.call_with_err_code('../contrib/macdeploy/macdeployqtplus neblio-Qt.app -add-qt-tr da,de,es,hu,ru,uk,zh_CN,zh_TW -dmg -fancy ../contrib/macdeploy/fancy.plist -verbose 3')
 
