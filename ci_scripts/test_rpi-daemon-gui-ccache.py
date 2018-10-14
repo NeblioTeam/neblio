20b267ef7ba6e45b9e5081baadf0d122ffb13eae
NeblioTeam <neblioteam@gmail.com>
2018-10-13 23:15:03 -0400

    Test RPi build without ccache

--- ci_scripts/test_rpi-daemon-gui-ccache.py
+++ ci_scripts/test_rpi-daemon-gui-ccache.py
@@ -11,16 +11,16 @@ nci.mkdir_p(deploy_dir)
 os.chdir(deploy_dir)
 
 # Download our ccache file, if it does not exist, skip building
-try:
-  url = 'https://neblio-build-staging.ams3.digitaloceanspaces.com/' + build_cache_name
-  response = urllib2.urlopen(url)
-  with open(build_cache_name, 'wb') as f: f.write(response.read())
-except urllib2.HTTPError as err:
-  if err.code == 404:
-    print('ccache file does not exist remotely for this branch, skip building!')
-    exit(0)
-  else:
-    raise
+# try:
+#   url = 'https://neblio-build-staging.ams3.digitaloceanspaces.com/' + build_cache_name
+#   response = urllib2.urlopen(url)
+#   with open(build_cache_name, 'wb') as f: f.write(response.read())
+# except urllib2.HTTPError as err:
+#   if err.code == 404:
+#     print('ccache file does not exist remotely for this branch, skip building!')
+#     exit(0)
+#   else:
+#     raise
 
 # Install docker
 nci.call_with_err_code('curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh && rm get-docker.sh')
@@ -35,10 +35,10 @@ nci.call_with_err_code('tar -zxf ' + build_cache_name)
 nci.call_with_err_code('sudo docker run -e BRANCH=' + os.environ['TRAVIS_BRANCH'] + ' -v ' + deploy_dir + ':/root/deploy -t neblioteam/nebliod-build-ccache-rpi')
 
 # Package Binaries & Cache
-nci.call_with_err_code('tar -zcf ' + deploy_dir + build_cache_name + ' -C ' + deploy_dir + ' .ccache')
+# nci.call_with_err_code('tar -zcf ' + deploy_dir + build_cache_name + ' -C ' + deploy_dir + ' .ccache')
 nci.call_with_err_code('rm -rf ' + deploy_dir + '.ccache')
 
 file_name = '$(date +%Y-%m-%d)---' + os.environ['TRAVIS_BRANCH'] + '-' + os.environ['TRAVIS_COMMIT'][:7] + '---RPi-neblio-Qt-nebliod---raspbian-stretch.tar.gz'
-nci.call_with_err_code('tar -zcvf "' + file_name + '" neblio-qt nebliod')
+# nci.call_with_err_code('tar -zcvf "' + file_name + '" neblio-qt nebliod')
 nci.call_with_err_code('sudo rm -f neblio-qt && sudo rm -f nebliod')
 nci.call_with_err_code('echo "Binary package at ' + deploy_dir + file_name + '"')
