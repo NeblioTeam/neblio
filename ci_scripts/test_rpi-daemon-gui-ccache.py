fce027dde64778b65c82dfb405930015e34ea3f9
NeblioTeam <neblioteam@gmail.com>
2018-10-25 19:18:07 -0400

    Finalize timeout logic, always build even if no ccache file

--- ci_scripts/test_rpi-daemon-gui-ccache.py
+++ ci_scripts/test_rpi-daemon-gui-ccache.py
@@ -10,15 +10,16 @@ build_cache_name = 'rpi-build-ccache-' + os.environ['TRAVIS_BRANCH'] + '.tar.gz'
 nci.mkdir_p(deploy_dir)
 os.chdir(deploy_dir)
 
-# Download our ccache file, if it does not exist, skip building
+# Download our ccache file
+has_ccache = True
 try:
   url = 'https://neblio-build-staging.ams3.digitaloceanspaces.com/' + build_cache_name
   response = urllib2.urlopen(url)
   with open(build_cache_name, 'wb') as f: f.write(response.read())
 except urllib2.HTTPError as err:
   if err.code == 404:
-    print('ccache file does not exist remotely for this branch, skip building!')
-    exit(0)
+    print('ccache file does not exist remotely for this branch')
+    has_ccache = False
   else:
     raise
 
@@ -29,10 +30,11 @@ nci.call_with_err_code('curl -fsSL https://get.docker.com -o get-docker.sh && su
 nci.call_with_err_code('docker run --rm --privileged multiarch/qemu-user-static:register --reset')
 
 # Extract Build ccache File
-nci.call_with_err_code('tar -zxf ' + build_cache_name)
+if (has_ccache):
+  nci.call_with_err_code('tar -zxf ' + build_cache_name)
 
 # Start Docker Container to Build nebliod & neblio-Qt
-nci.call_with_err_code('timeout --signal=SIGKILL 5m sudo docker run -e BRANCH=' + os.environ['TRAVIS_BRANCH'] + ' -v ' + deploy_dir + ':/root/deploy -t neblioteam/nebliod-build-ccache-rpi')
+nci.call_with_err_code('timeout --signal=SIGKILL 45m sudo docker run -e BRANCH=' + os.environ['TRAVIS_BRANCH'] + ' -v ' + deploy_dir + ':/root/deploy -t neblioteam/nebliod-build-ccache-rpi')
 nci.call_with_err_code('sudo docker kill $(sudo docker ps -a -q)')
 
 # Package Cache
@@ -41,7 +43,8 @@ nci.call_with_err_code('sudo rm -rf ' + deploy_dir + '.ccache')
 
 file_name = '$(date +%Y-%m-%d)---' + os.environ['TRAVIS_BRANCH'] + '-' + os.environ['TRAVIS_COMMIT'][:7] + '---RPi-neblio-Qt-nebliod---raspbian-stretch.tar.gz'
 
-# Check if BOTH binaries exist before trying to package them. If both binaries do not exist, delete any that do exist, as we likely had a build timeout
+# Check if BOTH binaries exist before trying to package them.
+# If both binaries do not exist, delete any that do exist, as we had a build timeout
 if(os.path.isfile('neblio-qt') and os.path.isfile('nebliod')):
   nci.call_with_err_code('tar -zcvf "' + file_name + '" neblio-qt nebliod')
   nci.call_with_err_code('sudo rm -f neblio-qt && sudo rm -f nebliod')
