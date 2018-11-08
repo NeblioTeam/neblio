19af42df7c2b59d0682fbc2dd160abeee6e53e09
NeblioTeam <neblioteam@gmail.com>
2018-11-08 13:02:15 -0500

    env var must be a string

--- ci_scripts/test_osx-gui_wallet.py
+++ ci_scripts/test_osx-gui_wallet.py
@@ -19,7 +19,7 @@ nci.mkdir_p(build_dir)
 os.chdir(build_dir)
 
 # do not auto update homebrew as it is very slow
-os.environ['HOMEBREW_NO_AUTO_UPDATE'] = 1
+os.environ['HOMEBREW_NO_AUTO_UPDATE'] = '1'
 
 # set up ccache
 nci.call_with_err_code('brew fetch --retry ccache        && brew install ccache --force')
