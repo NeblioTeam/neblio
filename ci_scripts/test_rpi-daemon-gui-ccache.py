2d4bb52ebd570b375c091793a4faba6c6392b53d
NeblioTeam <neblioteam@gmail.com>
2018-11-08 12:06:48 -0500

    Adjust rpi ccache

--- ci_scripts/test_rpi-daemon-gui-ccache.py
+++ ci_scripts/test_rpi-daemon-gui-ccache.py
@@ -5,9 +5,8 @@ import neblio_ci_libs as nci
 
 working_dir = os.getcwd()
 deploy_dir = os.path.join(os.environ['TRAVIS_BUILD_DIR'],'deploy', '')
-build_cache_name = 'rpi-build-ccache-' + os.environ['TRAVIS_BRANCH'] + '.tar.gz'
 
-# If this is a PR, bail out because PRs cannot use ccache and just waste 45 mins running
+# If this is a PR, bail out instead of just wasting 45 mins running
 if (os.environ['TRAVIS_PULL_REQUEST'] != 'false'):
   print('Pull Requests are not built for RPi since ccache cannot be used!')
   exit(0)
@@ -15,36 +14,21 @@ if (os.environ['TRAVIS_PULL_REQUEST'] != 'false'):
 nci.mkdir_p(deploy_dir)
 os.chdir(deploy_dir)
 
-# Download our ccache file
-has_ccache = True
-try:
-  url = 'https://neblio-build-staging.ams3.digitaloceanspaces.com/' + build_cache_name
-  response = urllib2.urlopen(url)
-  with open(build_cache_name, 'wb') as f: f.write(response.read())
-except urllib2.HTTPError as err:
-  if err.code == 404:
-    print('ccache file does not exist remotely for this branch')
-    has_ccache = False
-  else:
-    raise
-
 # Install docker
 nci.call_with_err_code('curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh && rm get-docker.sh')
 
 # Prepare qemu
 nci.call_with_err_code('docker run --rm --privileged multiarch/qemu-user-static:register --reset')
 
-# Extract Build ccache File
-if (has_ccache):
-  nci.call_with_err_code('tar -zxf ' + build_cache_name)
+# move .ccache folder to our deploy directory
+nci.call_with_err_code('mv ' + os.path.join(os.environ['HOME'],'.ccache', '') + ' ' + os.path.join(deploy_dir,'.ccache', ''))
 
 # Start Docker Container to Build nebliod & neblio-Qt
 nci.call_with_err_code('timeout --signal=SIGKILL 42m sudo docker run -e BRANCH=' + os.environ['TRAVIS_BRANCH'] + ' -v ' + deploy_dir + ':/root/deploy -t neblioteam/nebliod-build-ccache-rpi')
 nci.call_with_err_code('sleep 15 && sudo docker kill $(sudo docker ps -q);exit 0')
 
-# Package Cache
-nci.call_with_err_code('tar -zcf ' + deploy_dir + build_cache_name + ' -C ' + deploy_dir + ' .ccache')
-nci.call_with_err_code('sudo rm -rf ' + deploy_dir + '.ccache')
+# move .ccache folder back to travis ccache dir
+nci.call_with_err_code('mv ' + os.path.join(deploy_dir,'.ccache', '') + ' ' + os.path.join(os.environ['HOME'],'.ccache', ''))
 
 file_name = '$(date +%Y-%m-%d)---' + os.environ['TRAVIS_BRANCH'] + '-' + os.environ['TRAVIS_COMMIT'][:7] + '---RPi-neblio-Qt-nebliod---raspbian-stretch.tar.gz'
 
