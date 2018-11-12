eddaa85f25ac2edcb902c2215a1b394e20f065d7
NeblioTeam <neblioteam@gmail.com>
2018-11-12 17:39:32 -0500

    deploy test

--- ci_scripts/test_rpi-daemon-gui.py
+++ ci_scripts/test_rpi-daemon-gui.py
@@ -24,13 +24,24 @@ nci.call_with_err_code('docker run --rm --privileged multiarch/qemu-user-static:
 nci.call_with_err_code('mv ' + os.path.join(os.environ['HOME'],'.ccache', '') + ' ' + os.path.join(deploy_dir,'.ccache', ''))
 
 # Start Docker Container to Build nebliod & neblio-Qt
-nci.call_with_err_code('timeout --signal=SIGKILL 42m sudo docker run -e BRANCH=' + os.environ['TRAVIS_BRANCH'] + ' -v ' + deploy_dir + ':/root/deploy -t neblioteam/nebliod-build-ccache-rpi')
+nci.call_with_err_code('timeout --signal=SIGKILL 2m sudo docker run -e BRANCH=' + os.environ['TRAVIS_BRANCH'] + ' -v ' + deploy_dir + ':/root/deploy -t neblioteam/nebliod-build-ccache-rpi')
 nci.call_with_err_code('sleep 15 && sudo docker kill $(sudo docker ps -q);exit 0')
 
 # move .ccache folder back to travis ccache dir
 nci.call_with_err_code('mv ' + os.path.join(deploy_dir,'.ccache', '') + ' ' + os.path.join(os.environ['HOME'],'.ccache', ''))
 
 file_name = '$(date +%Y-%m-%d)---' + os.environ['TRAVIS_BRANCH'] + '-' + os.environ['TRAVIS_COMMIT'][:7] + '---RPi-neblio-Qt-nebliod---raspbian-stretch.tar.gz'
+
+# since RPi jobs are run several times we need to be creative in our deployment as only the original job can post to GitHub Releases.
+# So, start the original job via the GitHub tag and pass its job ID to every subsequent job. Once the build is complete, the final build job
+# will restart the original job, which will handle the deployment to GitHub releases.
+deploy_job_id = os.eviron.get('TRAVIS_DEPLOY_JOB_ID', '0')
+travis_tag    = os.eviron.get('TRAVIS_TAG', '')
+# if travis tag is populated, this is the job ID we want
+if (travis_tag != '') {
+	deploy_job_id = os.eviron['TRAVIS_JOB_ID']
+}
+print('Debug: Job ID: ' + deploy_job_id)
 
 # Check if BOTH binaries exist before trying to package them.
 # If both binaries do not exist, delete any that do exist, as we had a build timeout
@@ -46,4 +57,4 @@ else:
     nci.call_with_err_code('sudo rm -f nebliod')
 
   nci.call_with_err_code('echo "Binaries neblio-qt and nebliod not found, likely due to a timeout, restarting job..."')
-  nci.call_with_err_code('curl -X POST -H "Content-Type: application/json" -H "Travis-API-Version: 3" -H "Accept: application/json" -H "Authorization: token ' + os.environ["TRAVIS_API_TOKEN"] + '" -d \'{"request":{"message":"RPi Build Restart","branch":"' + os.environ["TRAVIS_BRANCH"] + '","config":{"merge_mode":"deep_merge","env":{"matrix":["target_v=rpi_daemon_wallet"]}}}}\' \'https://api.travis-ci.org/repo/NeblioTeam%2Fneblio/requests\'')
+  nci.call_with_err_code('curl -X POST -H "Content-Type: application/json" -H "Travis-API-Version: 3" -H "Accept: application/json" -H "Authorization: token ' + os.environ["TRAVIS_API_TOKEN"] + '" -d \'{"request":{"message":"RPi Build Restart","branch":"' + os.environ["TRAVIS_BRANCH"] + '","config":{"merge_mode":"deep_merge","env":{"global":{"TRAVIS_DEPLOY_JOB_ID":"' + deploy_job_id + '"},"matrix":["target_v=rpi_daemon_wallet"]}}}}\' \'https://api.travis-ci.org/repo/NeblioTeam%2Fneblio/requests\'')
