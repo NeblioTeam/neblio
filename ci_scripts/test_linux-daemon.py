4b739450ca208e3d935062e42a7193d9173a2d84
Neblio Team <neblioteam@gmail.com>
2019-05-02 18:26:35 -0400

    Update test_linux-daemon.py

--- ci_scripts/test_linux-daemon.py
+++ ci_scripts/test_linux-daemon.py
@@ -47,8 +47,8 @@ os.chdir(build_dir)
 
 nci.call_with_err_code('ccache -s')
 
-nci.call_with_err_code('python $TRAVIS_BUILD_DIR/build_scripts/CompileOpenSSL-Linux.py')
-nci.call_with_err_code('python $TRAVIS_BUILD_DIR/build_scripts/CompileCurl-Linux.py')
+nci.call_with_err_code('python ../build_scripts/CompileOpenSSL-Linux.py')
+nci.call_with_err_code('python ../build_scripts/CompileCurl-Linux.py')
 
 os.environ['PKG_CONFIG_PATH'] = os.path.join(working_dir, build_dir, 'curl_build/lib/pkgconfig/')
 os.environ['OPENSSL_INCLUDE_PATH'] = os.path.join(working_dir, build_dir, 'openssl_build/include/')
