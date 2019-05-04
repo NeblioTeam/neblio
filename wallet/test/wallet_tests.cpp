cd688f6b3b23baf0fc746605b78432bff58d5d6a
TheQuantumPhysicist <info@afach.de>
2019-05-04 16:39:16 +0200

    Minor

--- wallet/test/wallet_tests.cpp
+++ wallet/test/wallet_tests.cpp
@@ -40,7 +40,7 @@ static void add_coin(int64_t nValue, int nAge = 6 * 24, bool fIsFromMe = false, 
 
 static void empty_wallet(void)
 {
-    BOOST_FOREACH (COutput output, vCoins)
+    for (COutput output : vCoins)
         delete output.tx;
     vCoins.clear();
 }
