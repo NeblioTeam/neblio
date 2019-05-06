5119945f66e841cc9c15d884655cb4bb3edcf891
NeblioTeam <neblioteam@gmail.com>
2019-05-06 11:50:50 -0400

    Update version.cpp to pull data from latest git commit

--- wallet/version.cpp
+++ wallet/version.cpp
@@ -39,8 +39,8 @@ const std::string CLIENT_NAME("Satoshi");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives.
 #define GIT_ARCHIVE 0
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID ""
-#    define GIT_COMMIT_DATE "$Format:%cD"
+#    define GIT_COMMIT_ID "$Format:%h$"
+#    define GIT_COMMIT_DATE "$Format:%cD$"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
