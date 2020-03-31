9615822b9a632d9ac04e0af8ed4641427c26d4ba
Samer Afach <info@afach.de>
2020-03-31 18:40:55 +0200

    Add rpc_decodescript.py test

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -100,7 +100,7 @@ BASE_SCRIPTS= [
 #    'feature_proxy.py',
 #    'rpc_signrawtransaction.py',
 #    'p2p_disconnect_ban.py',
-#    'rpc_decodescript.py',
+   'rpc_decodescript.py',
    'rpc_blockchain.py',
 #    'rpc_deprecated.py',
 #    'wallet_disable.py',
