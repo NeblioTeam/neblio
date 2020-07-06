c32ca0e72ecb1eb9f573e4c7105e95a8ee11e933
NeblioTeam <neblioteam@gmail.com>
2020-07-06 16:31:41 -0400

    Update test_osx-gui_wallet.py

--- ci_scripts/test_osx-gui_wallet.py
+++ ci_scripts/test_osx-gui_wallet.py
@@ -34,7 +34,6 @@ nci.call_with_err_code('brew uninstall --ignore-dependencies openssl || true')
 nci.call_with_err_code('brew uninstall --ignore-dependencies openssl@1.1 || true')
 nci.call_with_err_code('brew uninstall --ignore-dependencies qrencode || true')
 nci.call_with_err_code('brew uninstall --ignore-dependencies libsodium || true')
-# nci.call_with_err_code('brew uninstall --ignore-dependencies icu4c || true')
 
 # Install High Seirra Versions of Depeendencies, due to that being the minimum version we support
 #ccache https://bintray.com/homebrew/bottles/download_file?file_path=ccache-3.7.6.high_sierra.bottle.tar.gz
@@ -43,7 +42,7 @@ nci.call_retry_on_fail('brew install --force https://assets.nebl.io/dependencies
 nci.call_retry_on_fail('brew install --force https://assets.nebl.io/dependencies/macos/qt-5.13.2.high_sierra.bottle.tar.gz')
 #berkeley-db@4 https://bintray.com/homebrew/bottles/download_file?file_path=berkeley-db%404-4.8.30.high_sierra.bottle.1.tar.gz
 nci.call_retry_on_fail('brew install --force https://assets.nebl.io/dependencies/macos/berkeley-db%404-4.8.30.high_sierra.bottle.1.tar.gz')
-#boost https://homebrew.bintray.com/bottles/boost-1.72.0_2.high_sierra.bottle.tar.gz
+#boost https://homebrew.bintray.com/bottles/boost-1.72.0_3.high_sierra.bottle.tar.gz
 nci.call_retry_on_fail('brew install --force https://assets.nebl.io/dependencies/macos/boost-1.72.0_3.high_sierra.bottle.tar.gz')
 #miniupnpc https://bintray.com/homebrew/bottles/download_file?file_path=miniupnpc-2.1.high_sierra.bottle.tar.gz
 nci.call_retry_on_fail('brew install --force https://assets.nebl.io/dependencies/macos/miniupnpc-2.1.high_sierra.bottle.tar.gz')
@@ -55,8 +54,7 @@ nci.call_retry_on_fail('brew install --force https://assets.nebl.io/dependencies
 nci.call_retry_on_fail('brew install --force https://assets.nebl.io/dependencies/macos/qrencode-4.0.2.high_sierra.bottle.tar.gz')
 #libsodium https://bintray.com/homebrew/bottles/download_file?file_path=libsodium-1.0.18_1.high_sierra.bottle.tar.gz
 nci.call_retry_on_fail('brew install --force https://assets.nebl.io/dependencies/macos/libsodium-1.0.18_1.high_sierra.bottle.tar.gz')
-#icu4c https://homebrew.bintray.com/bottles/icu4c-66.1.high_sierra.bottle.tar.gz
-# nci.call_retry_on_fail('brew install --force https://assets.nebl.io/dependencies/macos/icu4c-66.1.high_sierra.bottle.tar.gz')
+
 
 # force relinking
 nci.call_with_err_code('brew unlink qt            && brew link --force --overwrite qt')
@@ -68,12 +66,9 @@ nci.call_with_err_code('brew unlink python        && brew link --force --overwri
 nci.call_with_err_code('brew unlink openssl@1.1   && brew link --force --overwrite openssl@1.1')
 nci.call_with_err_code('brew unlink qrencode      && brew link --force --overwrite qrencode')
 nci.call_with_err_code('brew unlink libsodium     && brew link --force --overwrite libsodium')
-# nci.call_with_err_code('brew unlink icu4c         && brew link --force --overwrite icu4c')
 
 
 nci.call_with_err_code('ccache -s')
-
-nci.call_with_err_code('ls -al /usr/local/opt/icu4c/lib/')
 
 # prepend ccache to the path, necessary since prior steps prepend things to the path
 os.environ['PATH'] = '/usr/local/opt/ccache/libexec:' + os.environ['PATH']
