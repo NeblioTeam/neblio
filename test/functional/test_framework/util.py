3f82ecc2f0facbff9b9c861f6839987087d4cc48
TheQuantumPhysicist <info@afach.de>
2021-02-12 22:01:27 +0100

    Update util.py
    Increase timeout

--- test/functional/test_framework/util.py
+++ test/functional/test_framework/util.py
@@ -361,7 +361,7 @@ def connect_nodes_bi(nodes, a, b):
     connect_nodes(nodes[a], b)
     connect_nodes(nodes[b], a)
 
-def sync_blocks(rpc_connections, *, wait=1, timeout=300):
+def sync_blocks(rpc_connections, *, wait=1, timeout=600):
     """
     Wait until everybody has the same tip.
 
