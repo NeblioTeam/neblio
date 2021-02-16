c788558fe28061c74af382a91bf2c9e0473d6cb2
Samer Afach <info@afach.de>
2021-02-16 17:47:28 +0100

    Remove unused functions in block index

--- wallet/blockindex.cpp
+++ wallet/blockindex.cpp
@@ -96,15 +96,3 @@ bool CBlockIndex::IsInMainChain(const ITxDB& txdb) const
 {
     return (pnext || this == txdb.GetBestBlockIndex().get());
 }
-
-bool CBlockIndex::IsSuperMajority(int minVersion, const CBlockIndex* pstart, unsigned int nRequired,
-                                  unsigned int nToCheck)
-{
-    unsigned int nFound = 0;
-    for (unsigned int i = 0; i < nToCheck && nFound < nRequired && pstart != NULL; i++) {
-        if (pstart->nVersion >= minVersion)
-            ++nFound;
-        pstart = boost::atomic_load(&pstart->pprev).get();
-    }
-    return (nFound >= nRequired);
-}
