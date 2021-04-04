f86927b33c85c9ae2ee8bcbf74dded755b306f4d
Neblio Team <neblioteam@gmail.com>
2021-04-03 20:06:53 -0400

    Update test_linux-daemon-cmake.py

--- ci_scripts/test_linux-daemon-cmake.py
+++ ci_scripts/test_linux-daemon-cmake.py
@@ -51,7 +51,7 @@ nci.install_packages_debian(packages_to_install)
 nci.mkdir_p(deploy_dir)
 os.chdir(build_dir)
 
-os.environ['CCACHE_MAXSIZE'] = "500M"
+os.environ['CCACHE_MAXSIZE'] = "700M"
 nci.call_with_err_code('ccache -s')
 nci.call_with_err_code('ccache -z')
 
