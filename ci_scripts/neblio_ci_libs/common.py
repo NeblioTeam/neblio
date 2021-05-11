fd2bc2d1415ac1db8bae9fc13989eb9bf29d03d6
NeblioTeam <neblioteam@gmail.com>
2021-05-11 10:06:31 -0400

    Tune cache sizes

--- ci_scripts/neblio_ci_libs/common.py
+++ ci_scripts/neblio_ci_libs/common.py
@@ -62,11 +62,9 @@ def setup_travis_or_gh_actions_env_vars():
 		os.environ['BRANCH'] = os.environ['GITHUB_REF'].rsplit('/', 1)[1]
 		os.environ['COMMIT'] = os.environ.get('GITHUB_SHA')
 		os.environ['CCACHE_DIR'] = os.path.join(os.environ['GITHUB_WORKSPACE'],'.ccache')
-		# os.environ['CCACHE_COMPRESS'] = "1"
-		# os.environ['CCACHE_COMPRESSLEVEL'] = "9"
 		#Test without ccache compression now that GitHub Actions compresses cahces automatically
 		os.environ['CCACHE_NOCOMPRESS'] = "1"
-		os.environ['CCACHE_MAXSIZE'] = "1024M"
+		os.environ['CCACHE_MAXSIZE'] = "750M"
 		os.environ['CPATH'] = '/Applications/Xcode_11.2.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include'
 
 
