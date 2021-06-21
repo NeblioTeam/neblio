8710bd2d8705829a616af2f74ada2d2b000b54b5
NeblioTeam <neblioteam@gmail.com>
2021-06-21 16:52:41 -0400

    Bump to v3.3.0-BETA1 for testing

--- wallet/version.cpp
+++ wallet/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("Satoshi");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   ""
+#define CLIENT_VERSION_SUFFIX   "-BETA1"
 
 // The following part of the code determines the CLIENT_BUILD variable.
 // Several mechanisms are used for this:
